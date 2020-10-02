a0bae6f97b2f92157ede2556dc579b8ac9014617
David Ivan Perez Salazar <david_ivan@outlook.com>
2020-10-02 16:02:29 -0500

    Fixed an issue with responsiveness

--- src/Components/NavigationBar/NavigationBar.tsx
+++ src/Components/NavigationBar/NavigationBar.tsx
@@ -10,19 +10,21 @@ export const NavigationBar: FunctionComponent = () => {
     const location = useLocation()
 
     return (
-        <Navbar className={`navbar navbar-expand-lg fixed-top navbar-dark navBar ${location.pathname !== '/' && ' bg-dark'}`}>
+        <Navbar collapseOnSelect expand="lg" className={`navbar navbar-expand-lg fixed-top navbar-dark navBar ${location.pathname !== '/' && ' bg-dark'}`}>
             <div className="container">
                 <Navbar.Brand href="/">@pod32g</Navbar.Brand>
-                <Nav className="ml-auto justify-content-end">
-                    {
-                        Routes.map(el => {
-                            return (
-                                el.visible && <Nav.Link active={location.pathname === el.path}><Link className="nav-link" to={el.path}>{el.name}</Link></Nav.Link>
-                            )
-                        })
-                    }
-                </Nav>
-
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto justify-content-end">
+                        {
+                            Routes.map(el => {
+                                return (
+                                    el.visible && <Nav.Link active={location.pathname === el.path}><Link className="nav-link" to={el.path}>{el.name}</Link></Nav.Link>
+                                )
+                            })
+                        }
+                    </Nav>
+                </Navbar.Collapse>
             </div>
         </Navbar>
     )
