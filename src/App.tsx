e47001db6deea3b4b1973a27df5d4fa2e14c8e82
David Ivan Perez Salazar <david_ivan@outlook.com>
2020-10-09 20:48:41 -0500

    Added google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,21 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import { NavigationBar } from './Components';
 import Routes from './Routes/Routes';
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from "history";
+
 
 function App() {
+
+  const history = createBrowserHistory();
+
+  ReactGA.initialize("G-5P84M8EG3G");
+
+  history.listen(location => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  })
+
   return (
     <BrowserRouter>
       <NavigationBar />
